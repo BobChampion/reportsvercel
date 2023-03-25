3fc90d3a6593314feb4470f09270341ebe599b67
BobChampion <bobthechamp41@gmail.com>
2023-03-25 11:50:10 +0400

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,10 @@ const { voluumnAuth } = require('./helpers/voluumn/axiosFunctions');
 const { doPuppetterTask, tests } = require('./helpers/voluumn/puppetterrFunctions');
 const { getStreamsWithClick } = require('./helpers/adspect/puppetteer');
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  // res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   res.send(tests);
 });
 
