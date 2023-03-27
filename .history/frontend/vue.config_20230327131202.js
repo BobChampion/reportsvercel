8e3c97c8795d3cc47e6794f10dfaf3a73577bc2a
BobChampion <bobthechamp41@gmail.com>
2023-03-27 13:12:31 +0400

    fixed15

--- .history/frontend/vue.config_20230327131202.js
+++ .history/frontend/vue.config_20230327131202.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: 'http://localhost:8081/',
   },
-});
+  async headers() {
+    return [
+  }
+})
